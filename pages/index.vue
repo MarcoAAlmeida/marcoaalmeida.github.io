fa4359f07c8e38d58b5c1222b821d5782691fff7
MarcoAAlmeida <marcoalmeida.dev.br@gmail.com>
2024-05-05 21:53:02 -0300

    fixed gh-actions build failure with <clientOnly> tag

--- pages/index.vue
+++ pages/index.vue
@@ -13,16 +13,16 @@ const value = ref('type SomeType= {}')
 </script>
 
 <template>
+  <ClientOnly>
     <el-tabs v-model="activeName" class="mt-15 mr-2 ml-2 demo-tabs">
       <el-tab-pane label="stack" name="stack">
-        <ClientOnly>
-          <TechTree />
-        </ClientOnly>
+        <TechTree />
       </el-tab-pane>
       <el-tab-pane label="raw" name="raw">
         <MonacoEditor v-model="value" class="min-h-80" lang="typescript" />
       </el-tab-pane>
     </el-tabs>
+  </ClientOnly>
 </template>
 
 <style></style>
